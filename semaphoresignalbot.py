68d3d15aec7c237ad169a4bd56d2cb6cc5e02cec
b3nks <github@postelio.de>
2021-04-25 03:04:08 +0200

    commit

--- semaphoresignalbot.py
+++ semaphoresignalbot.py
@@ -17,13 +17,17 @@ async def echo(ctx: ChatContext) -> None:
         with open(playerpath, 'r') as fp:
             player = json.load(fp)
     except FileNotFoundError:
-        print("file not found")
-
-
-    if ctx.message.get_body() == "start":
         player = {'number': ctx.message.source.number, 'node': 'start', 'inventory': ['alive']}
         with open(playerpath, 'w') as fp:
             json.dump(player, fp, sort_keys=True, indent=4)
+
+    if ctx.message.get_body() == "reset":
+        player = {'number': ctx.message.source.number, 'node': 'start', 'inventory': ['alive']}
+        with open(playerpath, 'w') as fp:
+            json.dump(player, fp, sort_keys=True, indent=4)
+        await ctx.message.reply("Phew - I just restarted ... strange")
+
+    if ctx.message.get_body() == "start":
         time.sleep(5)
         await ctx.message.reply("Hi, äh. moment")
         time.sleep(10)
@@ -32,22 +36,25 @@ async def echo(ctx: ChatContext) -> None:
         await ctx.message.reply("Bin sofort da. arggg...habs gleich und mach ein Foto!")
         time.sleep(5)
         await ctx.message.reply("so, sorry... ein chaos hier...hier. schreib pic")
-
-    if ctx.message.get_body() == "pic":
         '''
-        path = Path(__file__).parent.absolute() / "apod.jpg"
-        attachment = {"filename": str(path),
-                      "width": "100",
-                      "height": "100"}
-        print(attachment)
-
-        await ctx.message.reply(body="Irgend ne idee was das ist?", attachments=[attachment])
+        if ctx.message.get_body() == "pic":
+            path = Path(__file__).parent.absolute() / "apod.jpg"
+            attachment = {"filename": str(path),
+                          "width": "100",
+                          "height": "100"}
+            print(attachment)
+    
+            await ctx.message.reply(body="Irgend ne idee was das ist?", attachments=[attachment])
         '''
-    elif ctx.message.get_body() == "ping":
+    elif ctx.message.get_body() == "test":
         player['node'] = 'pinged'
         with open(playerpath, 'w') as fp:
             json.dump(player, fp, sort_keys=True, indent=4)
         await ctx.message.reply("Pong")
+
+    elif ctx.message.get_body() == "ping":
+        await ctx.message.reply("Pong")
+
     else:
         await ctx.message.reply("blablabla")
 
